5e6fe6cb4b38cc2c65a6148ea4a6b2ec7383bf4a
SanjayKumar8-8 <bscs2512313@szabist.pk>
2026-02-18 21:37:50 +0500

    Initial commit: Blood Donation System frontend and backend

---
+++ backend/server.js
@@ -0,0 +1,57 @@
+import express from "express";
+import mysql from "mysql2";
+import cors from "cors";
+
+const app = express();
+app.use(express.json()); //form data read karne ke liye
+app.use(cors());  // frontend se backend access ke liye (ports 5500 and 5000)
+
+const db = mysql.createConnection({ // address of mysql database
+    host : "localhost",
+    user : "root",
+    password : "",
+    database : "blood_donation_db"
+}); 
+ // connect database
+ db.connect((err) =>{
+    if(err){
+    console.log("Database Connection Failed!")
+    return; // it stops the program
+ }
+  console.log("MySql Connected");
+})
+app.post("/add-donor",(req,res)=>{
+    const{name , age , city ,contact , email, blood_group } = req.body;
+
+    const sql = `INSERT INTO donors (name,age,city,contact,email,blood_group)
+    VALUES (?,?,?,?,?,?);`
+
+    db.query(sql, [name,age,city,contact,email,blood_group], (err,result) =>{
+    if(err){
+        console.log(err);
+        res.json({ success : false , message : "Failed to add donor"});
+        return;
+    }
+     res.json({ success : true , message : "Donor added successfully"}); 
+ });
+  });
+  app.get("/search-donor",(req,res)=>{
+    const {blood_group,city} = req.query;
+
+    const sql = `SELECT * FROM donors
+WHERE blood_group = ?
+AND LOWER(city) = LOWER(?)
+`;
+
+    db.query(sql,[blood_group,city] ,(err,result) =>{
+        if(err){
+            console.log(err);
+            res.json( {success : false ,message:"Search Failed"} );
+            return;
+        }
+        res.json({ success : true , donors : result});
+    })
+  })
+app.listen(5000 , () => {
+    console.log("Server is Running");
+})
